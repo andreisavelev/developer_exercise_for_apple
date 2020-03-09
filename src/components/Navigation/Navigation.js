7d0df7150bb317741fc44a0a285887e9c2e0a633
Andrew Savelev <savelevcorr@gmail.com>
2020-03-10 00:17:30 +0300

    Render basic navigation

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+import classes from './Navigation.module.css';
+
+/**
+ * Navigation component renders list of NavigationItems
+ * @param props {object}
+ * @param props.items {object[]}
+ * @returns {object}
+ */
+const navigation = props => (
+    <nav className={classes.navigation}>
+        <ul>
+
+            {props.items.map(item => {
+                return (
+                    <li key={item.section}>
+                        <a href={item.section}>{item.label}k</a>
+                    </li>
+                );
+            })}
+        </ul>
+    </nav>
+);
+
+export default navigation;
